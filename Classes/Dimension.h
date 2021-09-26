434510105527219d24e88c189f3a3706559d7375
EchoHackCmd <EchoHackCmd@gmail.com>
2021-09-26 20:08:18 +0100

    Create Dimension.h

---
+++ Classes/Dimension.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDES_MCBE_SDK_CLASSES_DIMENSION
+#define INCLUDES_MCBE_SDK_CLASSES_DIMENSION
+
+class Dimension {
+public:
+    //
+};
+
+#endif /* INCLUDES_MCBE_SDK_CLASSES_DIMENSION */
